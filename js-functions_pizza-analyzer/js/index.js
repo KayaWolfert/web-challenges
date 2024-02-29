c38a3ec44ea4061e6561dfa0cf1eb493aed78284
Kaya Wolfert <kayawolfert@web.de>
2024-02-29 11:51:21 +0100

    update solution for js-pizza-analyzer

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,50 +9,47 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = pizzaInput1.value;
+  let pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1,pizzaSize2);
+  updatePizzaDisplay(pizza1,pizzaInput1.value);
+  updateOutputColor(pizzaInput1.value,pizzaInput2.value);
+  
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = pizzaInput1.value;
+  let pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1,pizzaSize2);
+  updatePizzaDisplay(pizza2,pizzaInput2.value);
+  updateOutputColor(pizzaInput1.value,pizzaInput2.value);
+  
 });
 
-// Task 1
-// define the function calculatePizzaGain here
+
 function calculatePizzaGain(diameter1,diameter2){
   let area1 = (diameter1 / 2) **2 *Math.PI ;
   let area2 = (diameter2 / 2) **2 *Math.PI ;
   let pizzaGain = (area2 - area1) / area1 * 100;
-
-  console.log("size of area 1 is: " +area1);
-  console.log("size of area 2 is: " +area2);
   output.textContent= Math.round(pizzaGain) ;
-  
   }
 
-  calculatePizzaGain(24,25);
-
-   /* 
-
-4. Set the `textContent` of the output element to that value. 
-Use `Math.round(calculatedValue)` to round the pizza gain to
-the next whole integer value.
-
-5. Inside both event listeners, save the current values of both 
-pizza inputs in two variables `pizzaSize1` and `pizzaSize2`
-   > 💡 You can use the queried elements and get their value 
-   like this: `pizzaInput.value`
-6. Now call the function and pass in the two pizza sizes as 
-the arguments.
+  
+  function updatePizzaDisplay (pizzaElement,newSize){
+    const width = (newSize / 24) *100 ;
+    pizzaElement.style.width = width + "px";
+  }
 
 
-// Task 2
-// define the function updatePizzaDisplay here
+  function updateOutputColor(size1,size2){
+    if (size1 > size2){
+      outputSection.style.backgroundColor = "var(--red)";
 
-// Task 3
-// define the function updateOutputColor here
-
-
-*/
+    } else {
+      outputSection.style.backgroundColor = "var(--green)";
+    }
+  }
+   
 
 
  
