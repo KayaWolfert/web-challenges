58bd33a5333937576c5663074d6a97efb6649473
Kaya Wolfert <kayawolfert@web.de>
2024-05-10 11:01:07 +0200

    change solution

--- react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
+++ react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -1,21 +1,14 @@
 import styled from "styled-components";
-export default function BoxesWithStyledComponents({$isBlack}){
 
-return <>
-<StyledBox > </StyledBox>
-<StyledBox $isBlack> </StyledBox>
-
-</>
-}
 
 const StyledBox = styled.div`
-width: 100px;
-height: 100px;
-background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
-margin: 2rem;
-&:hover { background-color: red;}
-
-
-
+  width: 100px;
+  height: 100px;
+  background-color: ${({ $isBlack }) => ($isBlack ? "black" : "green")};
+  margin: 2rem;
+  &:hover {
+    background-color: red;
+  }
 `;
 
+export default StyledBox;
--- react-styled-components_box/pages/index.js
+++ react-styled-components_box/pages/index.js
@@ -1,15 +1,14 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
-import BoxesWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
+import StyledBox from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
 import styled from "styled-components";
 
 export default function HomePage() {
   return (
     <div>
-       
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-      <BoxesWithStyledComponents/>
-      
+      <StyledBox $isBlack />
+      <StyledBox />
     </div>
   );
 }
